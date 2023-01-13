00752398ce638e83dbdb418e83e783e535ce9044
Meko Steinberg <steinberg.meko@outlook.com>
2023-01-12 21:55:18 -0800

    Footer add links

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Box, Container } from '@mui/system';
-import { Button, Card, CardActionArea, CardContent, CardMedia, Grid, Toolbar, Typography } from '@mui/material';
+import { Button, Toolbar, Typography } from '@mui/material';
 import { Link as MuiLink } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Layout({ children }) {
     return (
@@ -41,7 +43,7 @@ export default function Layout({ children }) {
             <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }}>
                 <Container maxWidth="lg">
                     <Typography variant="h6" align="center" gutterBottom>
-                        Designed and created by M.Steinberg
+                        Created & Designed M.Steinberg
                     </Typography>
                     <Typography
                         variant="subtitle1"
@@ -50,6 +52,8 @@ export default function Layout({ children }) {
                     >
                         Capstone Project - General Assembly Immersive
                     </Typography>
+                    <MuiLink href="https://www.linkedin.com/in/mekosteinberg"><LinkedInIcon color="primary" fontSize="large" /></MuiLink>
+                    <MuiLink href="https://github.com/mekosteinberg"><GitHubIcon color="primary" fontSize="large" /></MuiLink>
                 </Container>
             </Box>
         </>
